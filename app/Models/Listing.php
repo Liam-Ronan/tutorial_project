28a8f24899a21f72904478647cdd0988afb2aade
Liam Ronan <liamronan16@gmail.com>
2022-10-19 18:56:26 +0100

    Finished adding create form validation (2:23:00)

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,13 +9,15 @@ class Listing extends Model
 {
     use HasFactory;
 
-    public static function find($id) {
-        $listings = self::all();
+    public function scopeFilter($query, array $filters) {
+        if($filters['tag'] ?? false) {
+            $query->where('tags', 'like', '%' . request('tag') . '%');
+        }
 
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
+        if($filters['search'] ?? false) {
+            $query->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('description', 'like', '%' . request('search') . '%')
+            ->orWhere('tags', 'like', '%' . request('search') . '%');
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
@@ -25,16 +26,25 @@ Route::get('/dashboard', function () {
 
 /* require __DIR__.'/auth.php'; */
 
-Route::get('/', function () {
-    return view('listings', [
-        'heading'=> 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+//Common Resource Routes:
+// index - show all listings
+// show - show single listings
+// create - show form to create new listing
+// store - store new listing
+// edit - show form to edit listing
+// update - update listing
+// destroy - delete listing
 
-Route::get('/listings/{listing}', function(Listing $listing) {
-    return view('listing', [
-        'listing' => $listing
-    ]); 
+/* All listings */
+Route::get('/', [ListingController::class, 'index']);
 
-});
+/* Show Create Form */
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+/* Store listing data */
+Route::post('/listings', [ListingController::class, 'store']);
+
+/* Single Listing */
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+
